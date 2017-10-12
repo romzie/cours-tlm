6ab2cd33d9f89f37120fbc7c02ad5c05c243c769
fpetrot <frederic.petrot@univ-grenoble-alpes.fr>
2017-10-12 21:25:15 +0200

    Ajout d'exemples de templates

--- code/template/template.cpp
+++ code/template/template.cpp
@@ -7,25 +7,27 @@
 
 using namespace std;
 
-template<typename T1,
-	 typename T2>
-struct simple_pair {
-	T1 first;
-	T2 second;
-	void print () {
-		cout << "first = " << first << endl
-		     << "second = " << second << endl;
-	}
-};
+template<typename T1, typename T2>
+   struct simple_pair {
+      T1 first;
+      T2 second;
+      void print () {
+         cout << "first = "  << first  << endl
+              << "second = " << second << endl;
+      }
+   };
 
-int main() {
-	simple_pair<int, const char *> p;
-	p.first = 42;
-	p.second = "some string";
-	p.print();
+int main(void)
+{
+   simple_pair<int, const char *> p;
+   p.first = 42;
+   p.second = "some string";
+   p.print();
 
-	simple_pair<int, int> p2;
-	p2.first = 1;
-	p2.second = 2;
-	p2.print();
+   simple_pair<int, int> p2;
+   p2.first = 1;
+   p2.second = 2;
+   p2.print();
+
+   return 0;
 }
